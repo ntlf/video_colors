ee82c756e82ad7ec6defd44dcf09993a08e8a578
Antal Farkas <antal@tabeo.co.uk>
2023-03-11 18:45:28 +0100

    Add structured logging

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,8 @@ use serde::{Deserialize, Serialize};
 use std::fs;
 use std::sync::mpsc;
 use std::sync::{Arc, Mutex};
-use std::time::Instant;
 use threadpool::ThreadPool;
+use tracing::{debug, trace};
 
 fn get_frame_color(frame: &Mat) -> [u8; 3] {
     let mut rgb_frame = Mat::default();
@@ -24,6 +24,27 @@ struct Json {
 }
 
 pub fn write_colors_to_file(colors: &Vec<[u8; 3]>, path: &str) {
+    debug!(
+        colors = format!(
+            "[{}, ... {}]",
+            colors
+                .iter()
+                .take(3)
+                .map(|c| format!("{:?}", c))
+                .collect::<Vec<_>>()
+                .join(", "),
+            colors
+                .iter()
+                .rev()
+                .take(2)
+                .rev()
+                .map(|c| format!("{:?}", c))
+                .collect::<Vec<_>>()
+                .join(", ")
+        ),
+        path
+    );
+
     let json = Json {
         colors: colors.to_owned(),
     };
@@ -39,14 +60,11 @@ fn get_video_info(video: &VideoCapture) -> (i32, i32) {
 }
 
 pub fn extract_colors(input: &str) -> Vec<[u8; 3]> {
-    let timer = Instant::now();
-
     let mut video = VideoCapture::from_file(input, 0).unwrap();
+
     let (fps, frame_count) = get_video_info(&video);
 
-    println!("Non-threaded version");
-    println!("FPS: {}", fps);
-    println!("Frame count: {}", frame_count);
+    debug!(fps, frame_count);
 
     let mut colors = Vec::new();
 
@@ -58,31 +76,22 @@ pub fn extract_colors(input: &str) -> Vec<[u8; 3]> {
 
             let color = get_frame_color(&frame);
 
+            trace!(i, ?color);
+
             colors.push(color);
         } else {
             video.grab().unwrap();
         }
-
-        print!("{:.2}%\r", i as f64 / frame_count as f64 * 100.0);
     }
-
-    println!("Elapsed time: {:.2?}", timer.elapsed());
 
     colors
 }
 
-/**
- * TODO: This implementation contains a bug
- */
 pub fn extract_colors_threaded(input: &str) -> Vec<[u8; 3]> {
-    let timer = Instant::now();
-
     let video = Arc::new(Mutex::new(VideoCapture::from_file(input, 0).unwrap()));
     let (fps, frame_count) = get_video_info(&video.lock().unwrap());
 
-    println!("Threaded version");
-    println!("FPS: {}", fps);
-    println!("Frame count: {}", frame_count);
+    debug!(fps, frame_count);
 
     let n_workers = std::thread::available_parallelism().unwrap().get() - 1;
     let pool = ThreadPool::new(n_workers);
@@ -106,6 +115,8 @@ pub fn extract_colors_threaded(input: &str) -> Vec<[u8; 3]> {
 
                 let color = get_frame_color(&frame);
 
+                trace!(i, ?color);
+
                 tx.send((i, color)).unwrap();
             }
         });
@@ -113,17 +124,7 @@ pub fn extract_colors_threaded(input: &str) -> Vec<[u8; 3]> {
 
     drop(tx);
 
-    let mut messages = vec![];
-
-    for (i, m) in rx.iter().enumerate() {
-        print!(
-            "{:.2}%\r",
-            i as f64 / (frame_count as f64 / fps as f64) * 100.0
-        );
-
-        messages.push(m);
-    }
-
+    let mut messages = rx.iter().collect::<Vec<_>>();
     messages.sort_by(|a, b| a.0.cmp(&b.0));
 
     let colors = messages
@@ -131,20 +132,16 @@ pub fn extract_colors_threaded(input: &str) -> Vec<[u8; 3]> {
         .map(|(_, color)| color.to_owned())
         .collect::<Vec<_>>();
 
-    println!("Elapsed time: {:.2?}", timer.elapsed());
+    debug!(colors = ?colors);
 
     colors
 }
 
 pub fn extract_colors_threaded_chunks(input: &str) -> Vec<[u8; 3]> {
-    let timer = Instant::now();
-
     let video = VideoCapture::from_file(input, 0).unwrap();
     let (fps, frame_count) = get_video_info(&video);
 
-    println!("Threaded-Chunks version");
-    println!("FPS: {}", fps);
-    println!("Frame count: {}", frame_count);
+    debug!(fps, frame_count);
 
     let n_workers = std::thread::available_parallelism().unwrap().get() - 1;
     let pool = ThreadPool::new(n_workers);
@@ -157,6 +154,8 @@ pub fn extract_colors_threaded_chunks(input: &str) -> Vec<[u8; 3]> {
         .map(|chunk| chunk.to_vec())
         .collect::<Vec<_>>();
 
+    debug!(chunks = ?(chunks.iter().map(|chunk| (chunk[0]..chunk[chunk.len() - 1])).collect::<Vec<_>>()));
+
     for chunk in chunks {
         let tx = tx.clone();
         let input = input.to_owned();
@@ -165,12 +164,17 @@ pub fn extract_colors_threaded_chunks(input: &str) -> Vec<[u8; 3]> {
             let mut video = VideoCapture::from_file(&input, 0).unwrap();
             video.set(CAP_PROP_POS_FRAMES, chunk[0] as f64).unwrap();
 
+            debug!(chunk = ?(chunk[0]..chunk[chunk.len() - 1]));
+
             for i in chunk {
                 if i % fps == 0 {
                     let mut frame = Mat::default();
                     video.read(&mut frame).unwrap();
 
                     let color = get_frame_color(&frame);
+
+                    trace!(i, ?color);
+
                     tx.send((i, color)).unwrap();
                 } else {
                     video.grab().unwrap();
@@ -181,25 +185,13 @@ pub fn extract_colors_threaded_chunks(input: &str) -> Vec<[u8; 3]> {
 
     drop(tx);
 
-    let mut messages = vec![];
-
-    for (i, m) in rx.iter().enumerate() {
-        print!(
-            "{:.2}%\r",
-            i as f64 / (frame_count as f64 / fps as f64) * 100.0
-        );
-
-        messages.push(m);
-    }
-
+    let mut messages = rx.iter().collect::<Vec<_>>();
     messages.sort_by(|a, b| a.0.cmp(&b.0));
 
     let colors = messages
         .iter()
         .map(|(_, color)| color.to_owned())
         .collect::<Vec<_>>();
-
-    println!("Elapsed time: {:.2?}", timer.elapsed());
 
     colors
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use clap::Parser;
 use std::path::PathBuf;
+use std::time::Instant;
+use tracing::{debug, info, Level};
 use video_colors::{
     extract_colors, extract_colors_threaded, extract_colors_threaded_chunks, write_colors_to_file,
 };
@@ -21,10 +23,30 @@ struct Cli {
     /// Use threaded version of color extraction with chunks
     #[arg(short, long)]
     chunks: bool,
+
+    /// Turn debugging information on
+    #[arg(short, long, action = clap::ArgAction::Count)]
+    debug: u8,
 }
 
 fn main() {
+    let timer = Instant::now();
     let args = Cli::parse();
+
+    tracing_subscriber::fmt()
+        .compact()
+        .with_file(true)
+        .with_line_number(true)
+        .with_target(false)
+        .with_max_level(match args.debug {
+            0 => Level::INFO,
+            1 => Level::DEBUG,
+            _ => Level::TRACE,
+        })
+        .with_thread_ids(true)
+        .init();
+
+    info!("Extracting colors from {}", args.input.display());
 
     let input = args.input.into_os_string().into_string().unwrap();
     let output = args
@@ -33,6 +55,13 @@ fn main() {
         .into_os_string()
         .into_string()
         .unwrap();
+
+    debug!(
+        input,
+        output,
+        threaded = args.threaded,
+        chunks = args.chunks
+    );
 
     let colors = if args.threaded {
         if args.chunks {
@@ -45,4 +74,6 @@ fn main() {
     };
 
     write_colors_to_file(&colors, &output);
+
+    info!("Done in {:.2?}", timer.elapsed());
 }
